20fa4c7b031adcbd79c1a11faf592c11768e2b79
Elizabeth Rogers <beth3346@gmail.com>
2015-06-07 22:47:43 -0500

    updated gitignore

---
+++ dist/elr-calendar.js
@@ -0,0 +1,12 @@
+(function($) {
+    window.drmCalendar = function(params) {
+        var self = {},
+            spec = params || {};
+
+        if ( 9 === 10 ) {
+            console.log(spec, $);
+        }
+
+        return self;
+    };
+})(jQuery);
